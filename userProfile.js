28e66a463e08ab86d283caf3bc4de09b24dd4b10
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-11-04 19:55:38 +0800

    Initial Commit

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+let fullName = "Rona Mae Ancheta";  
+let age = 19;                 
+let favoriteNumber = 7;       
+let favoriteColor = "Pink";   
+
+// Log the user information to the console
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
